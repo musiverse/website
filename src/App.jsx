5910b0c2d6f64e33d9b06c7bfb80603be91cb574
Shahil <sahilaryan9211@gmail.com>
2023-11-15 11:19:37 +0530

    started services section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import Services from "./Components/Services";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Services />
     </div>
   );
 }
---
+++ src/Components/Services.jsx
@@ -0,0 +1,5 @@
+const Services = () => {
+  return <div className="bg-gray-800 h-screen w-full">Services</div>;
+};
+
+export default Services;
