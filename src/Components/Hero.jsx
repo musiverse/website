17947ecdd29ad900da33262935f762a720acf451
Shahil <sahilaryan9211@gmail.com>
2023-11-27 23:36:15 +0530

    UI

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,5 @@
 // import vediofinal from "../assets/vediofinal.mp4";
-import { TypeAnimation } from "react-type-animation";
+// import { TypeAnimation } from "react-type-animation";
 // import GradientBtn from "./GradientBtn";
 import final from "../assets/final.mp4";
 
@@ -19,28 +19,28 @@ const Hero = () => {
       <div className="absolute w-full h-full top-0 left-0 bg-gray-900/30"></div>
 
       {/* Content Container */}
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-gray-100 text-center w-4/5 mx-auto">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-gray-100 text-center font-custom w-4/5 mx-auto">
         {/* Headline */}
         <h1
           data-aos="fade-down"
           data-aos-delay="500"
-          className="text-4xl md:text-6xl lg:text-7xl font-bold mb-4 text-gray-300"
+          className="text-4xl text-left md:text-4xl lg:text-6xl font-bold mb-4 text-gray-300"
         >
-          Elevate Your{" "}
+          Find the Perfect{" "}
           <span className="text-transparent bg-clip-text bg-accent">
-            Moments
+            Musicians
           </span>{" "}
-          with Live Music
+          for Your Event
         </h1>
 
         {/* Subheadline */}
         <p
           data-aos="fade-down"
           data-aos-delay="600"
-          className="text-xl font-secondary md:text-xl lg:text-3xl mb-6 text-gray-200"
+          className="text-xl font-tertiary md:text-2xl text-left  text-gray-200"
         >
-          Discover the Power of Live Performances
-          <TypeAnimation
+          Discover talented musicians and music services for any occasion.{" "}
+          {/* <TypeAnimation
             className="ml-2 leading-10 text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-red-600"
             sequence={[
               // Same substring at the start will only be typed out once, initially
@@ -58,13 +58,22 @@ const Hero = () => {
             // style={{ fontSize: '2em', display: 'inline-block' }}
             deletionSpeed={10}
             repeat={Infinity}
-          />
+          /> */}
+        </p>
+        <p className="pt-2 text-xl font-semibold tracking-wider md:text-2xl text-left text-accent font-tertiary ">
+          {" "}
+          Download our app today!
         </p>
 
         {/* CTA Button */}
-        <button className="bg-gradient-to-br from-orange-500 via-black to-green-500 text-gray-300 px-7 py-2 text-xl rounded-3xl font-medium focus:ring ring-black ring-opacity-10 gradient element-to-rotate">
-          Experience Now
-        </button>
+        <div className="flex flex-col space-y-2 md:flex-row space-x-3 mt-3 items-center justify-start">
+          <button className="mt-2 bg-gradient-to-br from-orange-500 via-black to-green-500 text-gray-300 px-7 py-2 text-xl rounded-2xl font-medium focus:ring ring-black ring-opacity-10 gradient element-to-rotate">
+            Download Now
+          </button>
+          <button className="px-12 py-2 rounded-2xl text-xl text-center font-medium">
+            Learn More
+          </button>
+        </div>
         {/* Unique Selling Proposition */}
         {/* <p className="text-lg mt-4 font-secondary flex justify-center items-center">
           <AvatarGroup />
