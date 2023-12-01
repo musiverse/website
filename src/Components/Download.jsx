fc0e1782d8b5fb06a6d16c33bb6693f0eaee03b1
Shahil <sahilaryan9211@gmail.com>
2023-12-01 14:44:19 +0530

    updates

--- src/Components/Download.jsx
+++ src/Components/Download.jsx
@@ -1,20 +1,36 @@
 import down1 from "../assets/down1.png";
+import android from "../assets/android.png";
+import { AiOutlineApple } from "react-icons/ai";
 
 const Download = () => {
   return (
-    <div className="flex flex-col md:flex-row gradient-bg-services items-center ">
-      <div>
-        <img src={down1} alt="mobile" />
+    <div className="h-[75vh]  text-white/80 md:h-[60vh] flex flex-col md:flex-row bg-slate-950 items-center border-y-2 border-purple-600 ">
+      <div className="ml-0 py-2 md:ml-[120px] overflow-hidden">
+        <img src={down1} alt="mobile" className="h-[300px] " />
       </div>
-      <div className="w-[780px] text-white font-tertiary ">
-        <p className="font-medium  text-2xl">Download Application</p>
-        <p className="w-[300px]">
-          Download our mobile app for iOS and android and enjoy unlimited access
-          to your favorite music, anytime and anywhere
+      <div className="overflow-hidden p-3 w-[340px] md:w-[780px] sm:ml-0  font-tertiary md:ml-[420px] space-y-7 tracking-wider">
+        <p className="font-medium text-center md:text-left text-3xl overflow-y-hidden font-new">
+          Download Application
         </p>
-        <div>
-          <button>Download</button>
-          <button>Download</button>
+        <p className="sm:w-[10px] text-center md:text-left md:w-[550px] sm:text-base md:text-xl ">
+          Download our mobile app for iOS and android and enjoy unlimited access
+          to your favorite music, anytime and anywhere .
+        </p>
+        <div className="flex flex-col w-[300px] md:w-[300px] justify-center items-center space-y-3 md:space-y-0 md:space-x-3 md:flex-row md:items-center">
+          {/* Android Button */}
+          <button className="px-1 py-1 rounded-3xl bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white">
+            <span className="text-base flex items-center bg-slate-950 hover:bg-slate-800 rounded-3xl px-4 py-[6px]">
+              Download
+              <img src={android} alt="android" className="h-5 w-7 pl-2" />
+            </span>
+          </button>
+          {/* Apple Button */}
+          <button className="px-1 py-1 rounded-3xl bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white">
+            <span className="text-base flex items-center bg-slate-950 hover:bg-slate-800 rounded-3xl px-4 py-[6px]">
+              Download
+              <AiOutlineApple size={25} className="pl-1" />
+            </span>
+          </button>
         </div>
       </div>
     </div>
--- src/Components/Vedio.jsx
+++ src/Components/Vedio.jsx
@@ -8,10 +8,7 @@ const Vedio = () => {
   return (
     <div className="bg-gradient-to-l from-slate-700 to-gray-900 py-6">
       <div className="container mx-auto">
-        <div
-          data-aos="fade-down"
-          className="flex flex-col justify-center items-center text-center lg:flex-row lg:justify-around"
-        >
+        <div className="flex flex-col justify-center items-center text-center lg:flex-row lg:justify-around">
           <h3 className="text-center md:text-left text-xl md:text-2xl font-medium mb-8 text-gray-300 lg:mb-0 w-[380px] md:w-[800px] pl-3 pr-3">
             Surprise Moments, Unforgettable Memories - Hire a Singer in Just 5
             Minutes!
@@ -27,7 +24,7 @@ const Vedio = () => {
           <div>
             <div
               onClick={() => setIsOpen(!isOpen)}
-              className=" cursor-pointer bg-videoBg bg-no-repeat bg-cover  w-[270px] h-[180px] flex items-center justify-center"
+              className="overflow-hidden cursor-pointer bg-videoBg bg-no-repeat bg-cover  w-[270px] h-[180px] flex items-center justify-center"
             >
               <BsPlayCircleFill className="text-4xl text-white/80 hover:text-white hover:scale-110 transition" />
             </div>
