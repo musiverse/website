bd92bd6dc897b408fca94c1b34a975900c73bf46
Shahil <sahilaryan9211@gmail.com>
2023-12-09 15:09:19 +0530

    ui

--- src/Components/Singers.jsx
+++ src/Components/Singers.jsx
@@ -5,7 +5,7 @@ import { FaCube } from "react-icons/fa6";
 const Singers = () => {
   return (
     <div className="overflow-y-hidden gradient-bg-services text-white">
-      <div className="p-5 h-[100vh] md:h-[80vh] mx-auto max-w-7xl space-x-0 md:space-x-10 flex flex-col md:flex-row justify-center overflow-y-hidden">
+      <div className="p-5 h-[120vh] md:h-[80vh] mx-auto max-w-7xl space-x-0 md:space-x-10 flex flex-col md:flex-row justify-center overflow-y-hidden">
         <div className=" w-[100%] md:w-[50%]  flex flex-col justify-center items-start ">
           <h1 className="text-3xl md:text-4xl w-[350px] md:w-[600px] overflow-y-hidden tracking-wide font-sans">
             Discover the Perfect Musicians for Your Event with just a few taps.
@@ -16,21 +16,21 @@ const Singers = () => {
           </p>
           <div className="mt-4 text-base md:text-lg space-y-3 flex flex-col font-sans">
             {" "}
-            <p className="flex items-center gap-4">
+            <p className="flex items-center gap-4 overflow-y-hidden">
               <FaCube size={13} />
               Browse a Variety of Musicians and Genres.{" "}
             </p>
-            <p className="flex items-center gap-4">
+            <p className="flex items-center gap-4 overflow-y-hidden">
               <FaCube size={13} />
               Book Musicians at Your Preffered Date and Time.
             </p>
-            <p className="flex items-center gap-4">
+            <p className="flex items-center gap-4 overflow-y-hidden">
               <FaCube size={13} />
               Enjoy Seamless and Reliable Booking Process.
             </p>
           </div>
         </div>
-        <div className="pt-[80px] flex items-start justify-start mx-auto">
+        <div className="pt-[80px] flex items-start justify-start mx-auto overflow-y-hidden">
           <img src={image4} alt="Singers" height={500} width={600} />
         </div>
       </div>
