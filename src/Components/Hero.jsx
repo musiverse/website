01b2572ade641dad7e1837341b10fb584eccd032
Shahil <sahilaryan9211@gmail.com>
2024-04-25 21:59:32 +0530

    ui

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,14 +6,26 @@ const Hero = () => {
       className="relative flex flex-col h-full w-full bg-[#030014]"
       id="about-me"
     >
+      {/* Video for large screens */}
       <video
         autoPlay
         muted
         loop
-        className="rotate-180 absolute top-[-340px]  h-full w-full left-0 Z-[-1] object-cover hidden md:block"
+        className="rotate-180 absolute top-[-340px] h-full w-full left-0 z-10 object-cover hidden md:block"
       >
         <source src="/blackhole.webm" type="video/webm" />
       </video>
+
+      {/* Video for small screens */}
+      <video
+        autoPlay
+        muted
+        loop
+        className="object-cover w-full h-full block md:hidden z-0 relative"
+      >
+        <source src="/mobbg.mp4" type="video/mp4" />
+      </video>
+
       <HeroContent />
     </div>
   );
